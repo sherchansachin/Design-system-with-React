50053840b6f2931f4b8205888ee5115f0ebb1e0b
Sachin Sherchan <sachinsherchan3@gmail.com>
2022-05-25 11:19:34 +0545

    update buttons docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
-    "@storybook/addon-docs",
     {
       name: "@storybook/addon-docs",
       options: {
